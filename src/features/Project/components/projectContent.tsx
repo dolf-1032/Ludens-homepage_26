80e7f5f2b5c44789ac187a4367d67b2653b28d77
그림자_군주_조연우 <ckhlight2@gmail.com>
2026-02-19 00:36:09 +0900

    유튜브 버그, 크기 수정

--- src/features/Project/components/projectContent.tsx
+++ src/features/Project/components/projectContent.tsx
@@ -7,8 +7,11 @@ type ProjectContentProps = {
 // MDX 안에서 사용되는 HTML 태그들에 자동으로 디자인을 입혀주는 설정입니다. 👇
 const mdxComponents = {
     iframe: (props: any) => (
-        <div className="relative w-full aspect-video mb-8 rounded-xl overflow-hidden shadow-lg">
-            <iframe {...props} className="absolute top-0 left-0 w-full h-full" />
+        <div className="relative w-full aspect-video my-12 rounded-2xl overflow-hidden shadow-2xl border border-white/5">
+            <iframe 
+                {...props} 
+                className="absolute top-0 left-0 w-full h-full" 
+            />
         </div>
     ),
 };
@@ -18,7 +21,7 @@ export default function ProjectContent({ content }: ProjectContentProps) {
         <>
             <p className="text-base md:text-lg text-white/50 font-semibold mb-4">프로젝트 설명</p>
             <section className="prose prose-invert lg:prose-lg max-w-none">
-                <MDXRemote source={content} />
+                <MDXRemote source={content} components={mdxComponents} />
             </section>
         </>
     )
